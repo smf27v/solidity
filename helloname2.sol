7c3b6b63cb193bfcf536c3a34d46a2c6e3992ea6
Sergio Martínez <sergio@MacBook-Pro-de-Sergio.local>
2017-10-09 17:30:26 +0200

    Lesson 2

---
+++ helloname2.sol
@@ -0,0 +1,22 @@
+pragma solidity ^0.4.0; //Solidity Version
+
+contract HelloWorldContract {  //Create a Contract
+    string name = 'Hello Sergio';  //Create a variable string
+    address owner;
+
+    // internal: keyword to indicate that the function can only be called by the smart contract itself, not externally.
+    function HelloWorldContract() internal{ //Create a function to initialize the contract.
+        owner = msg.sender;
+    }
+    function getName() public constant returns(string){  //Create a function for return the name.  Declare public for all ussers to acces
+        return name;
+    }
+
+    function setName(string newName) public returns(string){ //Create a function for select the newName.  It is not constant because we are going to modify.
+        if(owner != msg.sender){
+            return "You aren't the creator of the contract";
+        }
+        name = newName;
+        return "Yes you're the creator of the contract";
+    }
+}
